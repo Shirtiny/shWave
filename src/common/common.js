dde1dc271c80dab7539ff3e9bf73727ed5b9b31e
Shirtiny <1849468240@qq.com>
2020-04-11 22:35:28 +0800

    加点注释，调整merge

--- src/common/common.js
+++ src/common/common.js
@@ -22,15 +22,15 @@ export function throttle(fn, wait, option = { trailing: true }) {
   return _.throttle(fn, wait, option);
 }
 
-//合并
+//合并 传入两个unit8数组
 export function merge(...buffers) {
-  const Cons = buffers[0].constructor;
+  const Constructor = buffers[0].constructor;
   return buffers.reduce((pre, val) => {
-      const merge = new Cons((pre.byteLength | 0) + (val.byteLength | 0));
+      const merge = new Constructor((pre.byteLength | 0) + (val.byteLength | 0));
       merge.set(pre, 0);
       merge.set(val, pre.byteLength | 0);
       return merge;
-  }, new Cons());
+  }, new Constructor());
 }
 
 export default {
--- src/componets/parse.jsx
+++ src/componets/parse.jsx
@@ -4,7 +4,8 @@ import common from "../common/common";
 
 const Parse = ({ url }) => {
   const [dataSize, setDataSize] = useState(0);
-  const [data, setData] = useState(new Uint8Array());
+  //data为Uint8Array 8位无符号整型数组
+  const [data, setData] = useState(null);
 
   const fetchMedia = useCallback(() => {
     if (!url) return;
@@ -16,8 +17,11 @@ const Parse = ({ url }) => {
         const fileSize = res.headers.get("content-length");
         setDataSize(fileSize);
         logger.clog(res, fileSize);
+        //获取 ReadableStream 的 reader , ReadableStream接口呈现了一个可读取的二进制流操作 Fetch 通过Response的属性 body提供了一个具体的 ReadableStream 对象。
         reader = res.body.getReader();
+        // 8位无符号整型数组 Uint8Array与可读流ReadableStream 的协同用法。
         let data = new Uint8Array();
+        // "done"是一个布尔型，"value"是一个Unit8Array
         reader.read().then(function read({ done, value }) {
           if (done) {
             logger.clog("载入完成：", data);
@@ -26,6 +30,7 @@ const Parse = ({ url }) => {
           }
           logger.clog("载入ing：", data);
           data = common.merge(data, value);
+          //递归
           return reader.read().then(read);
         });
       }
